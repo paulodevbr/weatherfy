2420d647c06d46c69d8ac5693c13e9ac0c3181a7
Paulo de Oliveira Neto <paulo-gyn@outlook.com>
2021-02-27 19:49:20 -0300

    Add current weather layout to dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -2,9 +2,18 @@ import React from 'react';
 import { Button, View } from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
 import { useLocation } from '../../hooks/location';
-import { CurrentWeatherView, Container } from './styles';
-import CloudIcon from '../../components/Icons/CloudIcon';
+import {
+  CurrentWeatherView,
+  Container,
+  CurrentWeatherInfo,
+  MainTemp,
+  ImgTemp,
+  TextTitle,
+  TextSubtitle,
+  WeekTemps,
+} from './styles';
 import { colors } from '../../styles/colors';
+import SunIcon from '../../components/Icons/SunIcon';
 
 const Dashboard: React.FC = () => {
   const { clearLocation } = useLocation();
@@ -13,8 +22,40 @@ const Dashboard: React.FC = () => {
       <Container>
         <Button title="Sair" onPress={() => clearLocation()} />
         <CurrentWeatherView>
-          <CloudIcon />
+          <CurrentWeatherInfo>
+            <TextTitle>Goiânia</TextTitle>
+            <MainTemp>35°</MainTemp>
+            <View>
+              <TextTitle>Ensolarado</TextTitle>
+              <TextSubtitle>Sensação térmica 38°</TextSubtitle>
+            </View>
+          </CurrentWeatherInfo>
+          <ImgTemp>
+            <SunIcon height="300px" />
+          </ImgTemp>
         </CurrentWeatherView>
+        <WeekTemps>
+          <View>
+            <TextSubtitle>2:00</TextSubtitle>
+            <TextTitle>35°</TextTitle>
+          </View>
+          <View>
+            <TextSubtitle>3:00</TextSubtitle>
+            <TextTitle>35°</TextTitle>
+          </View>
+          <View>
+            <TextSubtitle>4:00</TextSubtitle>
+            <TextTitle>30°</TextTitle>
+          </View>
+          <View>
+            <TextSubtitle>5:00</TextSubtitle>
+            <TextTitle>27°</TextTitle>
+          </View>
+          <View>
+            <TextSubtitle>6:00</TextSubtitle>
+            <TextTitle>28°</TextTitle>
+          </View>
+        </WeekTemps>
       </Container>
     </LinearGradient>
   );
--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -1,9 +1,55 @@
 import styled from 'styled-components/native';
-import { LinearGradient } from 'react-native-svg';
+import { Platform } from 'react-native';
 
 export const Container = styled.View`
   flex: 1;
-  margin: 30px;
+  padding: 30px 30px ${Platform.OS === 'android' ? 150 : 40}px;
 `;
 
-export const CurrentWeatherView = styled.View``;
+export const CurrentWeatherView = styled.View`
+  flex-direction: row;
+  align-items: flex-start;
+`;
+
+export const CurrentWeatherInfo = styled.View`
+  flex: 1;
+  min-width: 40px;
+  min-height: 340px;
+  align-items: flex-start;
+  justify-content: space-between;
+`;
+
+export const TextTitle = styled.Text`
+  font-size: 26px;
+  color: #fff;
+`;
+
+export const TextSubtitle = styled.Text`
+  font-size: 18px;
+  color: #fff;
+`;
+
+export const MainTemp = styled.Text`
+  font-size: 80px;
+  font-weight: 600;
+  color: #fff;
+`;
+
+export const ImgTemp = styled.View`
+  flex: 1;
+  position: relative;
+  left: -20%;
+`;
+
+export const WeekTemps = styled.View`
+  width: 100%;
+  align-items: flex-start;
+  justify-content: space-between;
+  padding-top: 18px;
+  padding-bottom: 18px;
+  border-top-width: 0.5px;
+  border-top-color: #fff;
+  border-bottom-width: 0.5px;
+  border-bottom-color: #fff;
+  flex-direction: row;
+`;
